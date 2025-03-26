f0628a6bfbeb8ccf7a8b27546dbaac2549a24bc4
Aragho <Oluwadanny19@gmail.com>
2025-03-26 21:30:22 +0100

    code

--- src/Page/Homescreen1.jsx
+++ src/Page/Homescreen1.jsx
@@ -1,6 +1,3 @@
-
-
-
 import React, { useState, useRef, useEffect } from "react";
 import logo from "../assets/logo.png";
 import { FaRegUserCircle } from "react-icons/fa";
@@ -11,9 +8,6 @@ import { IoMdAddCircleOutline } from "react-icons/io";
 import { PiCreditCard } from "react-icons/pi";
 import { BsSend } from "react-icons/bs";
 import { MdNavigateNext } from "react-icons/md";
-import coin from "../assets/coins.png";
-import card from "../assets/card.png";
-import free from "../assets/free.png";
 import { IoCall } from "react-icons/io5";
 import { IoIosFootball } from "react-icons/io";
 import { RiArrowUpDownLine } from "react-icons/ri";
@@ -21,9 +15,12 @@ import { FaClockRotateLeft } from "react-icons/fa6";
 import { GoHomeFill } from "react-icons/go";
 import { BsBarChartFill } from "react-icons/bs";
 import { FiGrid } from "react-icons/fi";
+import { GiCoins } from "react-icons/gi";
+import { GiMoneyStack } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 
-const Homescreen1 = () => {
+const Homescreen1 = ({addTransaction}) => {
   const [activeIndex, setActiveIndex] = useState(null);
   const [indicatorPosition, setIndicatorPosition] = useState(0);
   const itemsRef = useRef([]);
@@ -56,17 +53,17 @@ const Homescreen1 = () => {
     {
       title: "Add Money",
       description: "Get more from your account",
-      image: coin
+      icon: <GiCoins />
     },
     {
       title: "Create a debit card",
       description: "Get more from your account",
-      image: card
+      icon: <PiCreditCard />
     },
     {
       title: "Earn ₦2,000 for inviting friends to FastEGO",
       description: "Get more from your account",
-      image: free
+      icon: <GiMoneyStack />
     },
   ];
 
@@ -88,39 +85,47 @@ const Homescreen1 = () => {
     <div className="flex flex-col items-center justify-center px-4 sm:px-6 md:px-8 lg:px-12 py-6 w-full max-w-screen-xl mx-auto">
       <div className="relative w-full max-w-5xl min-h-[40vh] md:min-h-[50vh] lg:min-h-[60vh] bg-gradient-to-l from-[#22A1F7] to-[#002F73] rounded-tl-3xl rounded-tr-3xl shadow-lg p-6">
     
-        <div className="flex items-center justify-between text-white">
-          <div className="flex items-center gap-3">
-            <FaRegUserCircle className="text-2xl" />
-            <div>
-              <h1 className="text-lg font-semibold leading-[120%] tracking-[-2%]">
-                Hi, Mystery Miracle
-              </h1>
-              <h3 className="text-sm font-light">Welcome, let's start making payments</h3>
-            </div>
-          </div>
-          <div className="flex items-center gap-4">
-            <BiHide className="text-2xl cursor-pointer" />
-            <IoMdNotificationsOutline className="text-2xl cursor-pointer" />
-          </div>
-        </div>
+      <div className="flex items-center justify-between text-white">
+  <div className="flex items-center gap-3">
+    <FaRegUserCircle className="text-2xl lg:text-4xl" />
+    <div>
+      <h1 className="text-lg lg:text-2xl font-semibold leading-[120%] tracking-[-2%]">
+        Hi, Mystery Miracle
+      </h1>
+      <Link to="home2"><h3 className="text-sm lg:text-lg font-light">
+        Welcome, let's start making payments
+      </h3></Link>
+    </div>
+  </div>
+  <div className="flex items-center gap-4">
+    <BiHide className="text-2xl lg:text-3xl cursor-pointer" />
+    <IoMdNotificationsOutline className="text-2xl lg:text-3xl cursor-pointer" />
+  </div>
+</div>
 
-        <div className="text-white mt-6">
-          <h2 className="font-thin leading-[150%] tracking-[-2%]">Total Balance</h2>
-          <div className="flex items-center justify-between">
-            <h1 className="text-2xl font-bold">₦0.00</h1>
-            <button aria-label="More options" className="w-12 h-12 shadow-md rounded-full flex items-center justify-center">
-  <HiDotsHorizontal className="text-2xl" />
-</button>
-
-          </div>
-          <h4 className="font-thin leading-[150%] tracking-[-2%]">Last updated 2 min ago.</h4>
-        </div>
+<div className="text-white mt-6">
+  <h2 className="font-thin text-base lg:text-xl leading-[150%] tracking-[-2%]">
+    Total Balance
+  </h2>
+  <div className="flex items-center justify-between">
+    <h1 className="text-2xl lg:text-4xl font-bold">₦0.00</h1>
+    <button
+      aria-label="More options"
+      className="w-12 h-12 lg:w-16 lg:h-16 shadow-md rounded-full flex items-center justify-center"
+    >
+      <HiDotsHorizontal className="text-2xl lg:text-3xl" />
+    </button>
+  </div>
+  <h4 className="font-thin text-sm lg:text-lg leading-[150%] tracking-[-2%]">
+    Last updated 2 min ago.
+  </h4>
+</div>
 
     
         <div className="absolute bottom-0 w-full max-w-[90%] md:max-w-[80%] lg:max-w-[95%] mx-auto h-14 md:h-16 lg:h-20 bg-[#3D87E6] rounded-tl-2xl rounded-tr-2xl overflow-hidden flex justify-between items-center px-6">
           <h1 className="text-white text-[20px] font-medium">FastEGO</h1>
-          <div className="w-96 h-44 bg-[#3D4364] rounded-full absolute -top-10 right-16 translate-x-[50%]  ">
-  <img src={logo} alt="Logo" className="w-16 h-40 object-contain ml-44" />
+          <div className="w-40 h-40 bg-[#3D4364] rounded-full absolute -top-10 right-16 translate-x-[50%]  ">
+  <img src={logo} alt="Logo" className="w-16 h-40 object-contain ml-10" />
 </div>
 
         </div>
@@ -164,7 +169,12 @@ const Homescreen1 = () => {
         <div className="flex items-center gap-4">
          
           <div className="w-[80px] h-[80px] bg-[#F1F5F9] rounded-full flex justify-center items-center">
-            <img src={section.image} alt={section.title} className="w-[60%] h-[60%] object-contain" />
+          <h1 className={`transition-all duration-300 ${
+  activeIndex === index ? "text-[#18A0FB]" : "text-[#527aae]"
+} text-xl sm:text-4xl font-bold`}>
+  {section.icon}
+</h1>
+
           </div>
 
          
@@ -215,7 +225,7 @@ const Homescreen1 = () => {
    </div>    
 
 
-   <p className="text-[#7D7C93]">You haven't made any transactions yet</p>
+   <p className="text-[#7D7C93] text-[13.5px] md:text-[25px]">You haven't made any transactions yet</p>
  </div>
  <div className="relative w-full flex flex-col mt-6 px-2 sm:px-4">
         <div className="absolute -top-1 left-0 right-0 w-full h-[3px] bg-gray-300 rounded-full">
